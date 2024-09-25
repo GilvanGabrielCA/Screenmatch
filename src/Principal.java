d217cc3a3358a7cad24c87248658e00d79c557d4
Gilvan Gabriel Correia de Alencar <gilvangabriealencar@gmail.com>
2024-09-25 15:12:43 -0400

    arraylist

--- src/Principal.java
+++ src/Principal.java
@@ -4,6 +4,8 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public  static void main(String[] args){
         Filme meuFilme = new Filme();
@@ -57,5 +59,20 @@ public class Principal {
         episodio.setSerie(lost);
         episodio.setTotalVisualizacoes(300);
         filtro.filtra(episodio);
+
+        var filmeDoGilvan = new Filme();
+        filmeDoGilvan.setNome("Ultimato");
+        filmeDoGilvan.setDuracaoEmMinutos(200);
+        filmeDoGilvan.setAnoDeLancamento(2003);
+        filmeDoGilvan.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoGilvan);
+        listaDeFilmes.add(meuFilme);
+        listaDeFilmes.add(outroFilme);
+
+        System.out.println("Tamanho da lista " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).toString());
+        System.out.println(listaDeFilmes);
     }
 }
