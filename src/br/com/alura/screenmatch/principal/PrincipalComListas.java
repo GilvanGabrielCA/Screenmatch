2d471676fe5fa2e34d3fae57f415296dfcfc77a0
Gilvan Gabriel Correia de Alencar <gilvangabriealencar@gmail.com>
2024-09-27 13:50:09 -0400

    List

--- src/br/com/alura/screenmatch/principal/PrincipalComListas.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -4,7 +4,7 @@ import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PrincipalComListas {
     public static void main(String[] args) {
@@ -12,7 +12,7 @@ public class PrincipalComListas {
         meuFilme.avalia(9);
         Filme outroFilme = new Filme("Avatar 2", 2023);
         outroFilme.avalia(10);
-        var filmeDoGilvan = new Filme("Ultimado", 2003);
+        var filmeDoGilvan = new Filme("Ultimado", 2023);
         filmeDoGilvan.avalia(6);
         Serie lost = new Serie("Lost", 2000);
         lost.avalia(9);
@@ -30,5 +30,28 @@ public class PrincipalComListas {
                 System.out.println("Classificação " + filme.getClassificacao());
             }
         }
+
+        List<String> buscaPorArtista = new LinkedList<>();
+        buscaPorArtista.add("Adam Sandler");
+        buscaPorArtista.add("Paulo Reis");
+        buscaPorArtista.add("Fernanda Montenegro");
+        buscaPorArtista.add("Chris Evans");
+        System.out.println(buscaPorArtista);
+
+        Collections.sort(buscaPorArtista);
+        System.out.println("Depois da ordenação: ");
+        System.out.println(buscaPorArtista);
+        System.out.println();
+        System.out.println("Lista de titulos ordenados");
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        System.out.println();
+
+        lista.sort(Comparator.comparing(Titulo::getAnoDeLancamento));
+        System.out.println("Ordenando por ano");
+        System.out.println(lista);
+
+
     }
 }
